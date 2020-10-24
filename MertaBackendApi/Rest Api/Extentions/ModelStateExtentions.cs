15a20c6cf7d108cb9e55833fc2a1f94e53172884
Taha Doğan <tahadgn@users.noreply.github.com>
2020-10-24 07:47:36 +0300

    Project is ready to code controllers and implementation

---
+++ MertaBackendApi/Rest Api/Extentions/ModelStateExtentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Api.Extentions
+{
+    public static class ModelStateExtentions
+    {
+        public static ICollection<string> GetErrorMessage(this ModelStateDictionary Dictionary)
+        {
+            return Dictionary.SelectMany(m => m.Value.Errors).Select(m => m.ErrorMessage).ToList();
+        }
+    }
+}
